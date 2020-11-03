708d6350b02cfdf8ded02f6e4ac96fdafcfc9bee
YosukeIMAI312 <litvyak119@yahoo.co.jp>
2020-11-03 17:24:54 +0900

    落し物の登録をできるように、落とし物の編集は未完成

--- company/register.php
+++ company/register.php
@@ -1,9 +1,69 @@
 <?php
+include('../assets/functions.php');
+if(isset($message)){
+    echo $message;
+}
 if (isset($_POST['id'])) {
+    //編集
     $id = $_POST['id'];
-    echo '<h1>ここは' . $id . 'の編集</h1>';
+    $title = "編集";
 } else {
-    echo '<h1>新規登録</h1>';
+    //登録
+    $title = "登録";
 }
 require 'header.php';
-//ようすけさん
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>落し物<?php print($title); ?> </title>
+</head>
+<body>
+    <div>
+        <h1>落し物<?php print($title); ?></h1>
+
+        <form action="update.php" method="POST">
+        <table>
+            <tr>
+                <th>名前</th>
+                <td>
+                    <input type="text" name="name" placeholder="名前を入力してください" size="25" maxlength="100" value="<?php if(isset($id))//データベースからnameを持ってくる ?>" required>
+                </td>
+            </tr>
+            <tr>
+                <th>詳細</th>
+                <td>
+                    <textarea name="details" placeholder="落し物の詳細を入力してください" rows="6" cols="60" value=""><?php if(isset($id))//データベースからdetailsを持ってくる?></textarea>
+                </td>
+            </tr>
+            <tr>
+                <th>カテゴリー</th>
+                <td>
+                    <select name="category" required>
+                         <option disabled selected value>未選択</option>
+                            <?php foreach($categorys as $category): ?>
+                                <!-- データベースから
+                                <?php //if($category == ) ?>
+                                -->
+                                <option value="<?php echo $category; ?>"><?php echo $category; ?></option>
+                            <?php endforeach; ?>         
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <th>発見時刻</th>
+                <td>
+                    <!--後々カレンダーから選択できるように 2020-10-20 13:00:00-->
+                    <input type="text" name="datetime" placeholder="発見時刻を入力してください" size="25" maxlength="100" value="<?php if(isset($id))//データベースからnameを持ってくる ?>" required>
+                </td>
+            </tr>
+        </table>
+        <input type="submit" value="<?php print($title); ?>">
+        </form>
+
+    </div>
+</body>
+</html>
