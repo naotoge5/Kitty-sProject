08e1be26e5d1bdc7b420e78bca7f418029446ffc
tauchi <st061959@m03.kyoto-kcg.ac.jp>
2020-11-17 15:53:27 +0900

    tauchi commits

--- assets/setupDB.sql
+++ assets/setupDB.sql
@@ -10,6 +10,7 @@ create user 'kitty'@'localhost' identified by 'pro02';
 
 /*mysqlデフォルト認証方式がcaching_sha2_password,PHPのMySQL接続方式が未対応のため*/
 grant all on kittydb.* to 'kitty'@'localhost';
+<<<<<<< Updated upstream
 
 /*権限の付与*/
 /*
@@ -33,3 +34,54 @@ create table objects (
     datetime datetime not null,
     company_id int not null
 );
+=======
+create table companies
+(
+    id             int auto_increment primary key,
+    name           varchar(100) not null,
+    tel            varchar(100) not null,
+    postal         varchar(100) not null,
+    address_first  varchar(100) not null,
+    address_second varchar(100) not null,
+    address_third  varchar(100) not null,
+    details        text,
+    mail           varchar(100) not null,
+    password       varchar(100) not null,
+    object_update  datetime
+);
+
+create table objects
+(
+    id         int auto_increment primary key,
+    name       varchar(100) not null,
+    details    text,
+    category   varchar(100) not null,
+    datetime   datetime,
+    company_id int          not null
+);
+
+
+insert into companies
+values (null, '京都コンピュータ学院 京都駅前校', '0120123456', '6018407', '京都府', '京都市南区', '西九条寺ノ前町10-5', '電話対応受付時間9:00-22:00',
+        '10naotoge5.ykputi@gmail.com',
+        '$2y$10$CstYoew/bQ0z7Yjb6T1wh.Z3wfynIEbQ5eySdwN8kyYaekPjfDSxC', null);
+insert into companies
+values (null, '京都情報大学院大学 百万遍キャンパス', '0120789111', '6068225', '京都府', '京都市左京区', '田中門前町7', null,
+        'st071959@m03.kyoto-kcg.ac.jp',
+        '$2y$10$CScjAAj9Cg4DspqQ1MubOe3t6bmy53TaN1GsrWaSahde/a9c4HTEa', null);
+insert into objects
+values (null, 'レインコート', '駐車場', '雨具類', '2020-11-20 13:00:00', 2);
+insert into objects
+values (null, '傘', '駐車場', '雨具類', '2020-11-20 14:00:00', 1);
+insert into objects
+values (null, '折り畳み傘', '駐車場', '雨具類', '2020-11-20 14:00:00', 2);
+insert into objects
+values (null, '折り畳み傘', '駐車場', '雨具類', '2020-11-20 14:00:00', 1);
+insert into objects
+values (null, '鉛筆', '駐車場', '手帳・文具類', '2020-11-20 14:00:00', 3);
+insert into objects
+values (null, '財布', '駐車場', '財布類', '2020-11-20 15:00:00', 2);
+insert into objects
+values (null, 'シャープペン', '駐車場', '手帳・文具類', '2020-11-21 16:00:00', 2);
+
+>>>>>>> Stashed changes
---
+++ public/search.php
@@ -0,0 +1,68 @@
+<?php
+
+$name=$_GET['name'];
+$prefecture=$_GET['prefectures'];
+$citie=$_GET['cities'];
+$town=$_GET['towns'];
+$categorie=$_GET['categories'];
+    /*var_dump($name);
+    var_dump($prefecture);
+    var_dump($citie);
+    var_dump($town);
+    var_dump($categorie);*/
+
+
+
+    if($prefecture!=='都道府県を選択してください'){
+        
+    }
+
+    if(isset()){
+        
+    }
+
+
+
+
+
+$_SESSION['companies']=search_name($name);
+header("Location:top.php");
+
+function search_name($name){//落とし物店舗名検索
+
+try {
+    include('../assets/functions.php');
+    $pdo = getPDO();//pdo取得
+    $stmt = $pdo->prepare("SELECT distinct companies.name  FROM objects JOIN companies ON objects.company_id=companies.id WHERE companies.name LIKE :name OR objects.name LIKE :name");
+    $stmt->bindValue(":name",'%'.$name.'%', PDO::PARAM_STR);
+    if ($stmt->execute()) {
+        return $stmt->fetchAll();
+    }
+    return false;
+} catch (PDOException $e) {
+    die($e->getMessage());
+}
+}
+
+
+function  category(){//カテゴリでの検索
+try{
+   $pdo = getPDO();//pdo取得
+   $sql='SELECT * FROM objects INNER JOIN  companies  on  company_id = id';
+   $pdo->query($sql);
+}catch(PDOException $e){
+        die($e->getMessage());
+}
+}
+
+
+
+function  prefectures(){//町域検索
+
+try{
+    $pdo = getPDO();//pdo取得
+}catch(PDOException $e){
+        die($e->getMessage());
+}
+}
+?>
