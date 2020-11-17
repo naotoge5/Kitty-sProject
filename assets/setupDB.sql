47bfac3da1646e30ef728395324c26d7dd927244
tauchi <st061959@m03.kyoto-kcg.ac.jp>
2020-11-17 15:54:56 +0900

    田内　変更

--- assets/setupDB.sql
+++ assets/setupDB.sql
@@ -10,31 +10,6 @@ create user 'kitty'@'localhost' identified by 'pro02';
 
 /*mysqlデフォルト認証方式がcaching_sha2_password,PHPのMySQL接続方式が未対応のため*/
 grant all on kittydb.* to 'kitty'@'localhost';
-<<<<<<< Updated upstream
-
-/*権限の付与*/
-/*
- grant all on pp11db.* to 'kitty'@'localhost' identified with mysql_native_password by 'password';
- MySQL5.7以降出来なくなった。
- */
-create table companies (
-    id int auto_increment primary key,
-    name varchar (100) not null,
-    tel varchar (100) not null,
-    address varchar (100) not null,
-    mail varchar (100) not null,
-    password varchar (100) not null
-);
-
-create table objects (
-    id int auto_increment primary key,
-    name varchar (100) not null,
-    details text,
-    category varchar(100) not null,
-    datetime datetime not null,
-    company_id int not null
-);
-=======
 create table companies
 (
     id             int auto_increment primary key,
@@ -84,4 +59,3 @@ values (null, '財布', '駐車場', '財布類', '2020-11-20 15:00:00', 2);
 insert into objects
 values (null, 'シャープペン', '駐車場', '手帳・文具類', '2020-11-21 16:00:00', 2);
 
->>>>>>> Stashed changes
