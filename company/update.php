793b8b4476249435ce81029bf0c1f5710a4e7eba
YosukeIMAI312 <litvyak119@yahoo.co.jp>
2020-11-17 14:56:35 +0900

    落し物の登録と修正の処理は完了

--- company/update.php
+++ company/update.php
@@ -1,26 +1,45 @@
 <?php
 try {
     include('../assets/functions.php');
+    //タイムゾーンを日本に設定
+    date_default_timezone_set('Asia/Tokyo');
    
     $pdo = getPDO();//pdo取得
 
-    //落し物の新規登録
-    //変数に格納
+    $id = $_POST['id'];
     $name = $_POST['name'];
     $details = $_POST['details'];
     $category = $_POST['category'];
     $datetime = $_POST['datetime'];
     $company_id = $_SESSION['id'];
-    //insert文の発行
-    $stmt = $pdo->prepare("INSERT INTO objects VALUES(null, :name, :details, :category, :datetime, :company_id)");
-    $stmt->execute(array(":name" => $name, ":details" => $details, ":category" => $category, ":datetime" => $datetime, ":company_id" => $company_id));
+    $object_update = date("Y-m-d H:i:s");
 
+    if(!empty($id)){
+      //落し物の編集
+      $stmt = $pdo->prepare("UPDATE objects SET name = :name, details = :details, category = :category, datetime = :datetime WHERE id = :id");
+      $stmt->execute(array(":name" => $name, ":details" => $details, ":category" => $category, ":datetime" => $datetime, ":id" => $id));
+
+      $company_id = $_SESSION['id'];
+      $stmt = $pdo->prepare("UPDATE companies SET object_update = :object_update WHERE id = :id");
+      $stmt->execute(array(":object_update" => $object_update, ":id" => $company_id));
+
+      $_SESSION['message'] = "落し物の編集が完了しました";
+    }else{
+      //落し物の新規登録
+      $stmt = $pdo->prepare("INSERT INTO objects VALUES(null, :name, :details, :category, :datetime, :company_id)");
+      $stmt->execute(array(":name" => $name, ":details" => $details, ":category" => $category, ":datetime" => $datetime, ":company_id" => $company_id));
+      
+      $stmt = $pdo->prepare("UPDATE companies SET object_update = :object_update WHERE id = :id");
+      $stmt->execute(array(":object_update" => $object_update, ":id" => $company_id));
+      
+      $_SESSION['message'] = "落し物の登録が完了しました";
+    }
     header("Location:management.php");
-
-    //落し物の編集
 
 } catch (PDOException $e) {
     die($e->getMessage());
+    $_SESSION['message'] = !empty($_POST['id']) ? "落し物の編集に失敗しました" : "落し物の登録に失敗しました";
+
     header("Location:register.php");
 }
 ?>
