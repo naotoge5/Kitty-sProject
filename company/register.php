291a6736e3502896069b52a3e4842dd99c07df61
YosukeIMAI312 <litvyak119@yahoo.co.jp>
2020-11-17 14:56:13 +0900

    セレクトの選択肢の修正とupdate.phpにpostされてきたidを送る

--- company/register.php
+++ company/register.php
@@ -1,8 +1,5 @@
 <?php
 include('header.php');
-if (isset($message)) {
-    echo $message;
-}
 if (isset($_POST['id'])) {
     //編集
     $id = $_POST['id'];
@@ -25,31 +22,35 @@ if (isset($_POST['id'])) {
     <h1>落し物<?php print($title); ?></h1>
 
     <form action="update.php" method="POST">
+    <input type="hidden" name="id" value="<?php if(isset($id))print($id); ?>">
         <table>
             <tr>
                 <th>名前</th>
                 <td>
                     <input type="text" name="name" placeholder="名前を入力してください" size="25" maxlength="100"
-                           value="<?php if (isset($id))//データベースからnameを持ってくる ?>" required>
+                           value="<?php if(isset($id))echo h(read_objectData($id)['name']); ?>" required>
                 </td>
             </tr>
             <tr>
                 <th>詳細</th>
                 <td>
                     <textarea name="details" placeholder="落し物の詳細を入力してください" rows="6" cols="60"
-                              value=""><?php if (isset($id))//データベースからdetailsを持ってくる?></textarea>
+                              value=""><?php if(isset($id))echo h(read_objectData($id)['details']); ?></textarea>
                 </td>
             </tr>
             <tr>
                 <th>カテゴリー</th>
                 <td>
                     <select name="category" required>
+                        <?php if(is_null($id)): ?>
                          <option disabled selected value>未選択</option>
-                            <?php foreach($categories as $category): ?>
-                                <!-- データベースから
-                                <?php //if($category == ) ?>
-                                -->
-                            <option value="<?php echo $category; ?>"><?php echo $category; ?></option>
+                        <?php endif; ?>
+                        <?php foreach($categories as $category): ?>
+                                <?php if($category == read_objectData($id)['category']): ?>
+                                    <option selected="selected" value="<?php echo $category ?>"><?php echo $category ?></option>
+                                <?php else: ?>
+                                    <option value="<?php echo $category; ?>"><?php echo $category; ?></option>
+                                <?php endif; ?>
                         <?php endforeach; ?>
                     </select>
                 </td>
@@ -58,14 +59,13 @@ if (isset($_POST['id'])) {
                 <th>発見時刻</th>
                 <td>
                     <!--後々カレンダーから選択できるように 2020-10-20 13:00:00-->
-                    <input type="text" name="datetime" placeholder="発見時刻を入力してください" size="25" maxlength="100"
-                           value="<?php if (isset($id))//データベースからnameを持ってくる ?>" required>
+                    <input type="datetime" name="datetime" placeholder="発見時刻を入力してください" size="25" maxlength="100"
+                           value="<?php if(isset($id))echo h(read_objectData($id)['datetime']); ?>" required>
                 </td>
             </tr>
         </table>
         <input type="submit" value="<?php print($title); ?>">
     </form>
-
 </div>
 </body>
 </html>
