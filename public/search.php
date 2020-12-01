ab3f872ba9b8937bcddb6d9f775f4d49735dc385
haruhiko yoshii <asr.yoshi@gmail.com>
2020-12-01 16:08:35 +0900

    ｊｊｊ

--- public/search.php
+++ public/search.php
@@ -1,14 +1,14 @@
 <?php
-include('../assets/functions.php');
+include '../assets/functions.php';
 
-$name = "%" . $_GET['name'] . "%";
+$name = $_GET['name'];
 $prefecture = isset($_GET['prefectures']) ? $_GET['prefectures'] : null;
 $city = $_GET['cities'];
-$town = $_GET['towns'] . '%';
+$town = $_GET['towns'];
 $category = $_GET['categories'];
 $query = "";
 $value = [];
-
+$keywords = [$name];
 
 if (is_null($prefecture)) {
     alert('不正なアクセスです', 'CAUTION');
@@ -17,16 +17,17 @@ if (is_null($prefecture)) {
         $query .= " and prefecture=:prefecture and city=:city and town  like :town";
         $value = array_merge(array(":prefecture" => $prefecture,
             ":city" => $city,
-            ":town" => $town
+            ":town" => $town . "%",
         ));
-
+        array_push($keywords, $prefecture . $city . $town);
     }
     if ($category !== "カテゴリーを選択してください") {
         $query .= "  and category=:category";
         $value = array_merge($value, array("category" => $category));
-
+        array_push($keywords, $category);
     }
-    $_SESSION['results'] = searchCompanies($query, $value);
+    $_SESSION['data']['keywords'] = $keywords;
+    $_SESSION['data']['results'] = searchCompanies($query, $value);
 }
 header("Location:result.php");
 
@@ -39,10 +40,10 @@ function searchCompanies(string $plus_query, array $plus_value)
 {
     global $name;
     $query = "select distinct companies.id, companies.name, companies.details from objects join companies on objects.company_id = companies.id where (companies.name like :name or objects.name like :name)" . $plus_query;
-    $value = array_merge(array(":name" => $name), $plus_value);
+    $value = array_merge(array(":name" => "%" . $name . "%"), $plus_value);
     print_r($value);
     try {
-        $pdo = getPDO();//pdo取得         
+        $pdo = getPDO(); //pdo取得
         $stmt = $pdo->prepare($query);
         $stmt->execute($value);
         return $stmt->fetchAll();
--- public/show.php
+++ public/show.php
@@ -1,36 +1,36 @@
 <?php
-include('../assets/functions.php');
+include '../assets/functions.php';
 $id = isset($_GET['id']) ? $_GET['id'] : 0;
 $company = readCompanyData($id);
 $objects = readObjectList($id);
 $title = '企業情報';
-include('../assets/_inc/header.php');
+include '../assets/_inc/header.php';
 ?>
 <main>
     <div class="container">
         <div class="card my-4">
             <div class="card-header">
-                <h4 class="card-title mb-0"><?= $name = h($company['name']) ?></h4>
+                <h4 class="card-title mb-0"><?=$name = h($company['name'])?></h4>
             </div>
             <div class="card-body">
                 <div class="row">
                     <div class="col-6">
                         <h5 class="card-title">住所</h5>
-                        <p class="card-text">〒&nbsp;<?= substr(h($company['postal']), 0, 3) . '-' . substr(h($company['postal']), -4) . "<br>" . h($company['prefecture']) . h($company['city']) . h($company['town']) ?></p>
+                        <p class="card-text">〒&nbsp;<?=substr(h($company['postal']), 0, 3) . '-' . substr(h($company['postal']), -4) . "<br>" . h($company['prefecture']) . h($company['city']) . h($company['town'])?></p>
                         <h5 class="card-title">電話番号</h5>
                         <?php if (strlen($company['tel']) === 10): ?>
-                            TEL：<a href="tel:<?= h($company['tel']) ?>"><?= substr(h($company['tel']), 0, 4) . '-' . substr(h($company['tel']), 4, 3) . '-' . substr(h($company['tel']), 7, 3) ?></a>
+                            TEL：<a href="tel:<?=h($company['tel'])?>"><?=substr(h($company['tel']), 0, 4) . '-' . substr(h($company['tel']), 4, 3) . '-' . substr(h($company['tel']), 7, 3)?></a>
                         <?php else: ?>
-                            TEL：<a href="tel:<?= h($company['tel']) ?>"><?= substr(h($company['tel']), 0, 3) . '-' . substr(h($company['tel']), 3, 4) . '-' . substr(h($company['tel']), 6, 4) ?></a>
-                        <?php endif; ?>
+                            TEL：<a href="tel:<?=h($company['tel'])?>"><?=substr(h($company['tel']), 0, 3) . '-' . substr(h($company['tel']), 3, 4) . '-' . substr(h($company['tel']), 6, 4)?></a>
+                        <?php endif;?>
                     </div>
                     <div class="col-6">
                         <h5 class="card-title">営業時間等</h5>
-                        <p class="card-text"><?= h($company['details']) ?></p>
+                        <p class="card-text"><?=h($company['details'])?></p>
                     </div>
                 </div>
                 <iframe width="100%" height="100%"
-                        src=" https://maps.google.co.jp/maps?output=embed&q=<?= h($company['name']) ?>"></iframe>
+                        src=" https://maps.google.co.jp/maps?output=embed&q=<?=h($company['name'])?>"></iframe>
             </div>
         </div>
         <div class="card my-4">
@@ -49,17 +49,17 @@ include('../assets/_inc/header.php');
                         <tbody>
                         <?php foreach ($objects as $row): ?>
                             <tr>
-                                <td><?= h($row['name']) ?></td>
-                                <td><?= date('Y年m月d日 H時i分', strtotime(h($row['datetime']))) ?></td>
+                                <td><?=h($row['name'])?></td>
+                                <td><?=date('Y年m月d日 H時i分', strtotime(h($row['datetime'])))?></td>
                             </tr>
-                        <?php endforeach; ?>
+                        <?php endforeach;?>
                         </tbody>
                     </table>
                 <?php else: ?>
                     <p class="mb-0">落とし物が登録されていません。</p>
-                <?php endif; ?>
+                <?php endif;?>
             </div>
         </div>
     </div>
 </main>
-<?php include('../assets/_inc/footer.php') ?>
+<?php include '../assets/_inc/footer.php'?>
