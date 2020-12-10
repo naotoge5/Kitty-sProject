d47b5b8253192a2a5f6eb6d0473d659679d445cf
naotoge5 <67360139+naotoge5@users.noreply.github.com>
2020-12-11 00:13:40 +0900

    ChangeHeaderMenu

--- assets/_inc/header.php
+++ assets/_inc/header.php
@@ -5,60 +5,54 @@ $level = preg_match('/authentication/', $sever_url) ? '../' : '';
 $link = preg_match('/public/', $sever_url) ? 'public' : 'company';
 $flag = true;
 ?>
-    <!DOCTYPE html>
-    <html lang="ja">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
-        <title><?= $title ?></title>
+<!DOCTYPE html>
+<html lang="ja">
 
-        <!-- css読み込み -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-        <link rel="stylesheet" href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.min.css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.min.css">
-        <link rel="stylesheet" href="../assets/css/bootstrap-datetimepicker.min.css">
-        <link rel="stylesheet" href="../assets/css/company.css">
-        <!-- 外部js読み込み -->
-        <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
-        <script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
-        <script src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap4.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/locale/ja.js"></script>
-        <script src="../assets/js/bootstrap-datetimepicker.min.js"></script>
-        <!-- 各js読み込み -->
-        <script src="<?= $level . '../assets/js/' . $link . '.js' ?>"></script>
-    </head>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
+    <title><?= $title ?></title>
+
+    <!-- css読み込み -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.min.css">
+    <link rel="stylesheet" href="../assets/css/bootstrap-datetimepicker.min.css">
+    <link rel="stylesheet" href="../assets/css/company.css">
+    <!-- 外部js読み込み -->
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap4.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/locale/ja.js"></script>
+    <script src="../assets/js/bootstrap-datetimepicker.min.js"></script>
+    <!-- 各js読み込み -->
+    <script src="<?= $level . '../assets/js/' . $link . '.js' ?>"></script>
+</head>
+
 <body>
     <header class="fixed-top">
-        <?php if (preg_match('/authentication/', $sever_url)):
+        <?php if (preg_match('/authentication/', $sever_url)) :
             unset($_SESSION['id']);
             if (isset($_POST['logout'])) {
                 alert('ログアウトしました', 'SUCCESS');
             } ?>
-        <?php else: ?>
+        <?php else : ?>
             <div class="pos-f-t">
-                <?php if (preg_match('/public/', $sever_url)): ?>
+                <?php if (preg_match('/public/', $sever_url)) : ?>
                     <!-- top, show -->
                     <div class="navbar navbar-dark bg-dark py-3">
                         <a class="navbar-brand" href="top.php">&nbsp;&nbsp;落とし物検索</a>
-                        <button class="navbar-toggler" type="button" data-toggle="collapse"
-                                data-target="#navbarContent"
-                                aria-controls="navbarContent"
-                                aria-expanded="false" aria-label="Toggle navigation">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>
                     </div>
                     <div class="collapse navbar-collapse bg-white" id="navbarContent">
                         <ul class="list-group list-group-flush">
-                            <a href="top.php"
-                               class="list-group-item list-group-item-action">検索ページ</a>
-                            <button class="list-group-item list-group-item-action"
-                                    data-toggle="collapse"
-                                    data-target="#favoriteContent"
-                                    aria-controls="favoriteContent"
-                                    aria-expanded="false">お気に入り
+                            <a href="top.php" class="list-group-item list-group-item-action">検索ページ</a>
+                            <button class="list-group-item list-group-item-action" data-toggle="collapse" data-target="#favoriteContent" aria-controls="favoriteContent" aria-expanded="false">お気に入り
                             </button>
                             <div class="collapse px-3 border-bottom" id="favoriteContent">
                                 <ul class="list-group list-group-flush">
@@ -68,7 +62,7 @@ $flag = true;
                             </div>
                         </ul>
                     </div>
-                <?php else: ?>
+                <?php else : ?>
                     <!-- management, register -->
                     <form method="POST" name="logout" action="authentication/login.php">
                         <input type="hidden" name="logout">
@@ -77,7 +71,7 @@ $flag = true;
                         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>
-                        <a class="navbar-brand" href="#">管理画面</a>
+                        <a class="navbar-brand" href="management.php">管理画面</a>
                     </nav>
                     <div class="collapse" id="navbarContent">
                         <?php
@@ -88,15 +82,12 @@ $flag = true;
                         }
                         ?>
                         <a href="management.php" class="list-group-item list-group-flush list-group-item-action">ホーム</a>
-                        <a href="#" class="list-group-item list-group-flush list-group-item-action">メール、パスワード変更</a>
-                        <a href="register.php" class="list-group-item list-group-flush list-group-item-action">新規登録</a>
-                        <a href="javascript:logout.submit()"
-                           class="list-group-item list-group-flush list-group-item-action">ログアウト</a>
+                        <a href="javascript:logout.submit()" class="list-group-item list-group-flush list-group-item-action">ログアウト</a>
                     </div>
                 <?php endif; ?>
             </div>
         <?php endif; ?>
-        <?php if (isset($_SESSION['alert'])): ?>
+        <?php if (isset($_SESSION['alert'])) : ?>
             <div class="<?= $_SESSION['alert']['class'] ?> fade show text-center mb-0" role="alert">
                 <strong><?= $_SESSION['alert']['message'] ?></strong>
                 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
@@ -111,17 +102,18 @@ $flag = true;
     </header>
     <div class="py-4">&nbsp;</div>
     <div class="py-4">&nbsp;</div>
-<?php if (!$flag): ?>
-    <main>
-        <div class="container">
-            <div class="card my-4">
-                <div class="card-header pb-0">
-                    <h3 class="card-title">読み込みエラー</h3>
-                </div>
-                <div class="card-body">
-                    <h4 class="card-subtitle">申し訳ございません、<br>しばらくしてからもう一度お試しください。</h4>
+    <?php if (!$flag) : ?>
+        <main>
+            <div class="container">
+                <div class="card my-4">
+                    <div class="card-header pb-0">
+                        <h3 class="card-title">読み込みエラー</h3>
+                    </div>
+                    <div class="card-body">
+                        <h4 class="card-subtitle">申し訳ございません、<br>しばらくしてからもう一度お試しください。</h4>
+                    </div>
                 </div>
             </div>
-        </div>
-    </main>
-    <?php exit; endif; ?>
+        </main>
+    <?php exit;
+    endif; ?>
--- company/management.php
+++ company/management.php
@@ -7,65 +7,66 @@ $objects = readObjectList($id);
 $title = '管理画面';
 include('../assets/_inc/header.php');
 ?>
-    <main>
-        <div class="container">
-            <div class="card">
-                <div class="card-header">
-                    <h3 class="card-title mb-0"><?= h($company['name']) ?></h3>
-                </div>
-                <div class="card-body d-none d-sm-block">
-                    <div class="row">
-                        <div class="col-6">
-                            <h5 class="mb-0 card-title">住所</h5>
-                            <p class="card-text">〒&nbsp;<?= h($company['postal']) . "<br>" . h($company['prefecture']) . h($company['city']) . h($company['town']) ?></p>
-                            <h5 class="mb-0 card-title">電話番号</h5>
-                            <p class="card-text">TEL：<a href="tel:<?= str_replace('-', '', h($company['tel'])) ?>"><?= h($company['tel']) ?></a></p>
-                        </div>
-                        <div class="col-6">
-                            <h5 class="mb-0 card-title ">営業時間等</h5>
-                            <textarea name="details" class="w-100 form-control"><?= h($company['details']); ?></textarea>
-                            <button id="update" type="button" class="btn btn-success btn-sm mt-2">更新</button>
-                        </div>
-                    </div>
-                    <iframe class="mt-2 " width="100%" height="100%"
-                            src=" https://maps.google.co.jp/maps?output=embed&q=<?= h($company['name']) ?>"></iframe>
-                </div>
-                <div class="card-body d-block d-sm-none">
-                    <p class="card-text">
-                        この画面ではご利用になれません
-                    </p>
-                </div>
+<main>
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title mb-0"><?= h($company['name']) ?></h3>
             </div>
-            <div class="card my-4 d-none d-sm-block">
-                <div class="card-header">
-                    <h4 class="card-title mb-0 d-inline"> 拾得物一覧 ／ </h4><h4 class="d-inline"><a href="register.php"
-                                                                                               class="text-decoration-none text-primary">新規追加</a>
-                    </h4>
+            <div class="card-body d-none d-sm-block">
+                <div class="row">
+                    <div class="col-6">
+                        <h5 class="mb-0 card-title">住所</h5>
+                        <p class="card-text">
+                            〒&nbsp;<?= h($company['postal']) . "<br>" . h($company['prefecture']) . h($company['city']) . h($company['town']) ?>
+                        </p>
+                        <h5 class="mb-0 card-title">電話番号</h5>
+                        <p class="card-text">TEL：<a href="tel:<?= str_replace('-', '', h($company['tel'])) ?>"><?= h($company['tel']) ?></a>
+                        </p>
+                    </div>
+                    <div class="col-6">
+                        <h5 class="mb-0 card-title ">営業時間等</h5>
+                        <textarea name="details" class="w-100 form-control"><?= h($company['details']); ?></textarea>
+                        <button id="update" type="button" class="btn btn-success btn-sm mt-2">更新</button>
+                    </div>
                 </div>
-                <div class="card-body">
-                    <?php if ($objects): ?>
-                        <table id="objects_table" class="table w-100">
-                            <thead>
+                <iframe class="mt-2 " width="100%" height="100%" src=" https://maps.google.co.jp/maps?output=embed&q=<?= h($company['name']) ?>"></iframe>
+            </div>
+            <div class="card-body d-block d-sm-none">
+                <p class="card-text">
+                    この画面ではご利用になれません
+                </p>
+            </div>
+        </div>
+        <div class="card my-4 d-none d-sm-block">
+            <div class="card-header">
+                <h4 class="card-title mb-0 d-inline"> 拾得物一覧 ／ </h4>
+                <h4 class="d-inline"><a href="register.php" class="text-decoration-none text-primary">新規追加</a>
+                </h4>
+            </div>
+            <div class="card-body">
+                <?php if ($objects) : ?>
+                    <table id="objects_table" class="table w-100">
+                        <thead>
                             <tr>
                                 <th>名前</th>
                                 <th>発見日時</th>
                             </tr>
-                            </thead>
-                            <tbody>
-                            <?php foreach ($objects as $row): ?>
-                                <tr class="edit list-group-item-action"
-                                    data-href="register.php?id=<?= h($row['id']) ?>">
+                        </thead>
+                        <tbody>
+                            <?php foreach ($objects as $row) : ?>
+                                <tr class="edit list-group-item-action" data-href="register.php?id=<?= h($row['id']) ?>">
                                     <td><?= h($row['name']) ?></td>
                                     <td><?= date('Y年m月d日 H時i分', strtotime(h($row['datetime']))) ?></td>
                                 </tr>
                             <?php endforeach; ?>
-                            </tbody>
-                        </table>
-                    <?php else: ?>
-                        <p class="mb-0">落とし物が登録されていません。</p>
-                    <?php endif; ?>
-                </div>
+                        </tbody>
+                    </table>
+                <?php else : ?>
+                    <p class="mb-0">落とし物が登録されていません。</p>
+                <?php endif; ?>
             </div>
         </div>
-    </main>
+    </div>
+</main>
 <?php include('../assets/_inc/footer.php') ?>
